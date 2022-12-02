00e4c008850a45c5e41757fa037b9fa1e1ff0d2c
arahisss <maindelay@mail.ru>
2022-12-02 18:49:38 +0300

    add auth and sign up

--- auth.php
+++ auth.php
@@ -1,43 +1,44 @@
 <?php require("header.html");
-use function UI\quit; 
- require("db.php");?>
+require("db.php");?>
 
 
             <div class="form-block form-auth">
-                <form action="#" class="form" method="post" enctype="multipart/form-data">
+                <form action="?index_auth.php" class="form" method="post" enctype="multipart/form-data">
+                        
                     <h1>Аутентификация</h1>
 
-                    <?php
-                        $result = mysqli_query($connect, "SELECT * FROM users WHERE
-                            login='".$_POST["login"]."' AND
-                            password='".md5($_POST["password"])."'
-                        ");
-
-                        session_start();
-                        $session_user = (isset($_SESSION["user"])) ? $_SESSION["user"] : false;
-
-
-                        ?>
-
                     <label  for="login">Логин</label>
-                    <input id="login" type="text" name="login">
+                    <input required id="login" type="text" name="login">
 
                     <label for="password">Пароль</label>
-                    <input type="password" name="password">
+                    <input required type="password" name="password">
+
+                    
 
                     <div class="but">
-
-                        <a href="<?php
-                            if(!$result || mysqli_num_rows($result) == 0){
-                                echo '#';
-                                exit;
-                            }
-                            echo 'index_auth.php';
-                        ?>">Отправить</a>
+                        <input type="submit" name="auth" id="button-submit">
                     </div>
                     
+                    <?php
+                    $result = mysqli_query($connect, "SELECT * FROM users WHERE
+                    login='".$_POST["login"]."' AND
+                    password='".md5($_POST["password"])."'
+                    "); // добавить шифрование
                     
+                    
+                    if(!$result || mysqli_num_rows($result) == 0){
+                        // echo "Неверный логин или пароль";
+                        exit;
+                    }
+                    
+                    session_start();
+                    $_SESSION["user"] = mysqli_fetch_assoc($result);
+                    
+                    header("Location: index_auth.php")
+                    ?>
                 </form>
+
+                
             </div>
 
 <?php require("footer.html"); ?>
---
+++ register.php
@@ -0,0 +1,42 @@
+<?php require("header.html");
+require("db.php");?>
+
+
+            <div class="form-block form-auth">
+                <?php
+                    if (!empty($_POST)){
+                        $result = mysqli_query($connect, "SELECT * FROM users WHERE login=\"".$_POST['login']."\"");
+                        //echo mysqli_num_rows($result);
+                        if(mysqli_num_rows($result) == 0){
+                            mysqli_query($connect, "INSERT INTO users (login, password) VALUES (
+                                \"".$_POST["login"]."\",
+                                \"".md5($_POST["password"])."\"
+                                )"
+                            );
+                        }
+                        //$id = mysqli_insert_id($connect);
+                        header("Location: index_auth.php");
+                    }
+                ?>
+                <form action="?index_auth.php" class="form" method="post" enctype="multipart/form-data">
+                        
+                    <h1>Регистрация</h1>
+
+                    <label  for="login">Логин</label>
+                    <input required id="login" type="text" name="login">
+
+                    <label for="password">Пароль</label>
+                    <input required type="password" name="password">
+
+                    
+
+                    <div class="but">
+                        <input type="submit" name="auth" id="button-submit">
+                    </div>
+                    
+                </form>
+
+                
+            </div>
+
+<?php require("footer.html"); ?>
